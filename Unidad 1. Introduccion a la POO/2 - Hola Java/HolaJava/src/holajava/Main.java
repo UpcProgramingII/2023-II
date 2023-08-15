f2a528c29e9c4421b518009c6401796929a41b3a
UpcProgramingII <142351191+UpcProgramingII@users.noreply.github.com>
2023-08-15 15:52:49 -0500

    Add files via upload

---
+++ Unidad 1. Introduccion a la POO/2 - Hola Java/HolaJava/src/holajava/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holajava;
+
+/**
+ *
+ * @author jSeoanesL
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        System.out.println("Bienvenidos al mundo Java");
+        System.out.println("Programacion II");
+        System.out.println("Sistemas - Unicesar");
+
+    }
+    
+}
