65577b46f0cf9f9561a658a39455404be8bbd7f4
UpcProgramingII <142351191+UpcProgramingII@users.noreply.github.com>
2023-10-24 17:34:57 -0500

    Add files via upload

---
+++ Unidad 6 - Interfaces graficas de usuario/swing-awt/EjercicioFacturaTresCapas/src/entidades/Factura.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Jairo F
+ */
+public abstract class Factura {
+    
+    private static int noConsecutivo=0;
+    private int consecutivo;
+    private LocalDate fechaFactura;
+    private double valorFactura;
+
+    public Factura() {
+        
+        noConsecutivo++;
+        this.consecutivo=noConsecutivo;
+    }
+
+    public Factura(LocalDate fechaFactura, double valorFactura) {
+        this.fechaFactura = fechaFactura;
+        this.valorFactura = valorFactura;
+        noConsecutivo++;
+        this.consecutivo=noConsecutivo;
+    }
+
+    /**
+     * @return the consecutivo
+     */
+    public int getConsecutivo() {
+        return consecutivo;
+    }
+
+    /**
+     * @param consecutivo the consecutivo to set
+     */
+    public void setConsecutivo(int consecutivo) {
+        this.consecutivo = consecutivo;
+    }
+
+    /**
+     * @return the fechaFactura
+     */
+    public LocalDate getFechaFactura() {
+        return fechaFactura;
+    }
+
+    /**
+     * @param fechaFactura the fechaFactura to set
+     */
+    public void setFechaFactura(LocalDate fechaFactura) {
+        this.fechaFactura = fechaFactura;
+    }
+
+    /**
+     * @return the valorFactura
+     */
+    public double getValorFactura() {
+        return valorFactura;
+    }
+
+    /**
+     * @param valorFactura the valorFactura to set
+     */
+    public void setValorFactura(double valorFactura) {
+        this.valorFactura = valorFactura;
+    }
+
+    public abstract String toString();    
+    
+    public abstract double pagoFactura();
+    
+    public String infoFactura(){
+        
+        return String.format("%-15s: %s\n%-15s: %.2f\n ", 
+                             "Fecha factura", this.fechaFactura.toString(),
+                             "Valor factura", this.valorFactura);
+        
+    }
+    
+    
+    
+}
